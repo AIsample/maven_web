6ef7de182e712bb40e49fbddece70badc15fe2e8
zuruzirou <zuruzirou@gmail.com>
2024-09-22 21:10:40 +0900

    a

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,13 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.autoconfigure.domain.EntityScan; // 追加
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // 追加
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.example.entity"})
 @ComponentScan(basePackages = {"com.example.controller", "com.example.service"})
-@EnableJpaRepositories(basePackages = {"com.example.repository"})
+@EnableJpaRepositories("com.example.repository")
+@EntityScan("com.example.entity")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
